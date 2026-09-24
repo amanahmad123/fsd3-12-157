94713d69eb26acfd1ee4bbc0fd8a2cba8b38b7b6
Aman Ahmad <YOUR_NOREPLY_EMAIL>
2026-09-24 12:07:37 +0530

    new

--- lab4/prg7.js
+++ lab4/prg7.js
@@ -48,3 +48,5 @@ const getUserById = (pid) => {
 users.
 }
 
+const updateUser = (pid, updateData)
+
--- lab5/app.js
+++ lab5/app.js
@@ -6,6 +6,15 @@ app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
+
+app.get("/home", (req, res) => {
+    res.send("Hey baby");
+});
+
+app.get("/contact", (req, res ) => {
+    res.send("kya hall bhai kai kya karha hia aja gahar par maze karte hai jo bhi hoga ddekha jayaega ")
+});
+
 app.listen(3003, () => {
     console.log("Server running on port 3000");
 });
